2ee3eac41f7eb178e11c798fa973776f3e4b400a
Marie Findlay <mariealicefindlay@gmail.com>
2019-02-03 18:03:37 +0000

    Updates

---
+++ precache-manifest.ddfd495a7f002905433a7c973f872924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "814fe85964ca5752881a6c8e06e3f4f8",
+    "url": "/MixReact/static/media/cocktails1.814fe859.jpg"
+  },
+  {
+    "revision": "b1e93c2f5f522705956b",
+    "url": "/MixReact/static/js/runtime~main.b1e93c2f.js"
+  },
+  {
+    "revision": "7dab2efd0d4bdbbeb8d1",
+    "url": "/MixReact/static/js/main.7dab2efd.chunk.js"
+  },
+  {
+    "revision": "8dee5e3708ecf4298970",
+    "url": "/MixReact/static/js/1.8dee5e37.chunk.js"
+  },
+  {
+    "revision": "7dab2efd0d4bdbbeb8d1",
+    "url": "/MixReact/static/css/main.42b3bdcd.chunk.css"
+  },
+  {
+    "revision": "1992b502af0db7b64cf9442cf52baaca",
+    "url": "/MixReact/index.html"
+  }
+];
